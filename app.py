8d92ecff62075efe87a6c0c48d7c52e041071f56
Regina McGuire <rmcguire@antelecom.net>
2020-05-27 11:41:47 -0700

    Add sqlite database

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'scores.db')
+
 
 @app.route('/')
 def hello_players():
-    return 'Hello Players'
-
+    return jsonify(message='Hello Players'),200
 
 
 if __name__ == '__main__':
