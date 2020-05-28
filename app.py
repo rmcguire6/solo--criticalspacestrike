bea06d62faf27ccb6c8bb95d3ce6cb4a91459e7d
Regina McGuire <rmcguire@antelecom.net>
2020-05-27 18:13:09 -0700

    Add flask cli commands to create, seed, and delete database

--- app.py
+++ app.py
@@ -9,6 +9,31 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'sc
 
 db = SQLAlchemy(app)
 
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created!')
+
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Database dropped!')
+
+@app.cli.command('db_test_seed')
+def db_seed():
+    score1 = Score(score='435')
+    score2 = Score(score='87')
+    score3 = Score(score='47')
+    score4 = Score(score='000')
+
+    db.session.add(score1)
+    db.session.add(score2)
+    db.session.add(score3)
+    db.session.add(score4)
+    db.session.commit()
+    print('Database seeded!')
+
 
 @app.route('/')
 def hello_players():
