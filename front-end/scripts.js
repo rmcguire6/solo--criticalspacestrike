1dc53d7313791f90c4d1ff47e09e372b35c2de92
Regina McGuire <rmcguire@antelecom.net>
2020-06-01 22:02:28 -0700

    Change spacing of enemy spaceships

--- front-end/scripts.js
+++ front-end/scripts.js
@@ -20,7 +20,7 @@ function draw_ship(spacing_x, spacing_y){
     let x = start_x + spacing_x
     let y = start_y + spacing_y
     let w = 100
-    let h = 100
+    let h = 90
     ship_image.onload = () => {
         ctx.drawImage(ship_image, x, y, w, h);
     }
@@ -28,26 +28,20 @@ function draw_ship(spacing_x, spacing_y){
 
 function draw() {
     let ctx = document.getElementById('screen').getContext('2d')
-     // create a spacing for y, increment by tile_height/width which is set to 50 atm.
-
     var tile_width = 100
-    var tile_height = 80
-    var x_spacing = tile_width + 20
-    var y_spacing = tile_height + 10
+    var tile_height = 100
+    var x_spacing = tile_width
+    var y_spacing = tile_height
 
     for (var i = 0; i < 3; i++) {
-
         for (var j = 0; j < 8; j++) {
           draw_ship(x_spacing, y_spacing)
           x_spacing += tile_width * 2
-
         }
-        // after inner for loop is done, reset x_spacing, and increment y_spacing
-        x_spacing = tile_width + 50
-        y_spacing += tile_height + 50
-
+        x_spacing = tile_width
+        y_spacing += tile_height + 30
       }
-      draw_ship( tile_width * 8, tile_height * 6)
+      draw_ship( tile_width * 8, tile_height * 5)
     }
 
 window.addEventListener('load', (event) => {
