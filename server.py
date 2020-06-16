1a2feaaa5c57f486fb8b462d72a3d34a1a5e747d
Regina McGuire <rmcguire@antelecom.net>
2020-06-15 22:17:25 -0700

    Refactor database code

--- server.py
+++ server.py
@@ -7,36 +7,14 @@ import os
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'scores.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-
-@app.cli.command('db_create')
-def db_create():
+@app.before_first_request
+def create_tables():
     db.create_all()
-    print('Database created!')
-
-
-@app.cli.command('db_drop')
-def db_drop():
-    db.drop_all()
-    print('Database dropped!')
-
-@app.cli.command('db_test_seed')
-def db_seed():
-    score1 = Score(score='435')
-    score2 = Score(score='87')
-    score3 = Score(score='47')
-    score4 = Score(score='000')
-
-    db.session.add(score1)
-    db.session.add(score2)
-    db.session.add(score3)
-    db.session.add(score4)
-    db.session.commit()
-    print('Database seeded!')
-
 
 @app.route('/')
 def play_game():
