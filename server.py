099d8b3cc29b9a6f3b2dbe86d9297a1fafe04a0f
Regina McGuire <rmcguire@antelecom.net>
2020-06-18 16:41:41 -0700

    Sort player list by score

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
 from flask_marshmallow import Marshmallow
+from operator import itemgetter
 import os
 
 app = Flask(__name__)
@@ -21,16 +22,26 @@ def play_game():
     return render_template('index.html'),200
 
 @app.route('/scores', methods=['POST','GET'])
-def scores(new_score=None):
+def scores():
+    new_score = None
+    player =  ''
     if request.method == 'POST':
         new_score = request.form['new_score']
         player = request.form['player']
+        if player == '':
+            player = 'Player'
         score = Score(score=new_score, player=player)
         db.session.add(score)
         db.session.commit()
-    scores_list = Score.query.all()
-    scores = scores_schema.dump(scores_list)
-    return render_template('scores.html', scores=scores, new_score=new_score)
+    scores = Score.query.all()
+    scores = scores_schema.dump(scores)
+    new_list = []
+    for score in scores:
+        element =(score['score'], score['player'], score[id])
+        new_list.append(element)
+    display = sorted(new_list, key=itemgetter(0),reverse=True)
+    display = display[0:5]
+    return render_template('scores.html', scores=display)
 
 # database models
 
@@ -48,6 +59,29 @@ class ScoreSchema(ma.Schema):
 
 score_schema = ScoreSchema()
 scores_schema = ScoreSchema(many=True)
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created!')
+
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Database dropped')
+
+@app.cli.command('db_seed')
+def db_seed():
+    score1 = Score(player='Player 1', score=3600)
+    score2 = Score(player='Player 2', score=1200)
+    score3 = Score(player='Player 3', score=1600)
+    db.session.add(score1)
+    db.session.add(score2)
+    db.session.add(score3)
+    db.session.commit()
+    print('Database seeded')
+
 
 if __name__ == '__main__':
     app.run()
